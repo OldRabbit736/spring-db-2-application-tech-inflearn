c744fe360e326ab9883f6fa401d9e8bab2bff63f
OldRabbit736 <sylvan0212@gmail.com>
2023-01-23 16:59:58 +0900

    완료: 데이터 접근 기술-MyBatis | MyBatis 적용1-기본

---
+++ follow-along/itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -0,0 +1,27 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 인터페이스 메서드를 호출하면 mapper xml 파일에서 해당 SQL 을 실행하게 된다.
+ * 구현체는 자동으로 생성된다.
+ */
+@Mapper
+public interface ItemMapper {
+    void save(Item item);
+
+    void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findAll(ItemSearchCond itemSearchCond);
+
+
+}
